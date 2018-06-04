c82ee2efc82e25acd82320a81061f2e2151a1c9e
Ewald Wasscher <ewald.wasscher@kpn.com>
2018-06-04 09:26:09 +0200

    Applied source code formatting

--- src/main/java/nl/logius/resource/pp/example/KeyUtil.java
+++ src/main/java/nl/logius/resource/pp/example/KeyUtil.java
@@ -4,9 +4,7 @@
  * Copyright: Logius (2018)
  * @author: Bram van Pelt 
  */
-
 package nl.logius.resource.pp.example;
-
 
 import java.io.File;
 import java.io.IOException;
@@ -26,77 +24,69 @@ import nl.logius.resource.pp.key.PseudonymDecryptKey;
 import org.apache.commons.io.FileUtils;
 
 public class KeyUtil {
-	
-	private IdentityDecryptKey decryptKey;
-	private EncryptedVerifiers verifiers;
-	private EncryptedVerifiers pVerifiers;
-	private PseudonymDecryptKey pDecryptKey;
-	private PseudonymClosingKey pClosingKey;
-	private String IDENTITY_POINT = "AmUppru04ghsI/FvbvV59eoX3lCUWlMAZKu1pPdlvixch5avV+aFwQg=";
-	private String PSEUDONYM_POINT = "A9GtKDUn++nl2NWtN4F/2id1gmBhxn4I6Qr9BfeMN+fjNuXGvE79qHc=";
-	
-	public KeyUtil() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
-	{
-         getIdentityKeys();
-         getPseudoKeys();
-	}
-	
-	private void getIdentityKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
-	{
-        // Convert P7 key to PEM
-        final InputStream is = KeyUtil.class.getResourceAsStream("/p7/ID-4.p7");
-            String identityKeyPem = CMS.read(getPrivateKey(), is);
-            // Convert PEM to IdentityDecryptKey
-            decryptKey = DecryptKey.fromPem(identityKeyPem, IdentityDecryptKey.class);
-            // Derive verifier (for signature verifying) from key
-            verifiers = decryptKey.toVerifiers(IDENTITY_POINT);
-	}
-	
-	private void getPseudoKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
-	{   
-        final InputStream isPd = KeyUtil.class.getResourceAsStream("/p7/PD-4.p7");
-        	String pseudoKeyPem = CMS.read(getPrivateKey(), isPd);
-            // Convert PEM to IdentityDecryptKey
-        	pDecryptKey = DecryptKey.fromPem(pseudoKeyPem, PseudonymDecryptKey.class);
-            // Derive verifier (for signature verifying) from key
-            pVerifiers = pDecryptKey.toVerifiers(PSEUDONYM_POINT);
-        
-        final InputStream isPc = KeyUtil.class.getResourceAsStream("/p7/PC-4.p7");
-        	String pseudoClosingKeyPem = CMS.read(getPrivateKey(), isPc);
-            // Convert PEM to IdentityDecryptKey
-        	pClosingKey = DecryptKey.fromPem(pseudoClosingKeyPem, PseudonymClosingKey.class);
 
-	}
-	
-	private static PrivateKey getPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-			File file = new File(Main.class.getClassLoader().getResource("private.p8").getFile());
-    	byte[] bytesArray = FileUtils.readFileToByteArray(file);
+  private IdentityDecryptKey decryptKey;
+  private EncryptedVerifiers verifiers;
+  private EncryptedVerifiers pVerifiers;
+  private PseudonymDecryptKey pDecryptKey;
+  private PseudonymClosingKey pClosingKey;
+  private String IDENTITY_POINT = "AmUppru04ghsI/FvbvV59eoX3lCUWlMAZKu1pPdlvixch5avV+aFwQg=";
+  private String PSEUDONYM_POINT = "A9GtKDUn++nl2NWtN4F/2id1gmBhxn4I6Qr9BfeMN+fjNuXGvE79qHc=";
 
-      return KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(bytesArray));
-    }
-		
-	public IdentityDecryptKey getDecryptKey()
-	{
-		return decryptKey;
-	}
-	
-	public EncryptedVerifiers getVerifiers()
-	{
-		return verifiers;
-	}
-	
-	public EncryptedVerifiers getPVerifiers()
-	{
-		return pVerifiers;
-	}
-	
-	public PseudonymDecryptKey getPDecryptKey()
-	{
-		return pDecryptKey;
-	}
-	
-	public PseudonymClosingKey getPClosingKey()
-	{
-		return pClosingKey;
-	}
+  public KeyUtil() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    getIdentityKeys();
+    getPseudoKeys();
+  }
+
+  private void getIdentityKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    // Convert P7 key to PEM
+    final InputStream is = KeyUtil.class.getResourceAsStream("/p7/ID-4.p7");
+    String identityKeyPem = CMS.read(getPrivateKey(), is);
+    // Convert PEM to IdentityDecryptKey
+    decryptKey = DecryptKey.fromPem(identityKeyPem, IdentityDecryptKey.class);
+    // Derive verifier (for signature verifying) from key
+    verifiers = decryptKey.toVerifiers(IDENTITY_POINT);
+  }
+
+  private void getPseudoKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    final InputStream isPd = KeyUtil.class.getResourceAsStream("/p7/PD-4.p7");
+    String pseudoKeyPem = CMS.read(getPrivateKey(), isPd);
+    // Convert PEM to IdentityDecryptKey
+    pDecryptKey = DecryptKey.fromPem(pseudoKeyPem, PseudonymDecryptKey.class);
+    // Derive verifier (for signature verifying) from key
+    pVerifiers = pDecryptKey.toVerifiers(PSEUDONYM_POINT);
+
+    final InputStream isPc = KeyUtil.class.getResourceAsStream("/p7/PC-4.p7");
+    String pseudoClosingKeyPem = CMS.read(getPrivateKey(), isPc);
+    // Convert PEM to IdentityDecryptKey
+    pClosingKey = DecryptKey.fromPem(pseudoClosingKeyPem, PseudonymClosingKey.class);
+
+  }
+
+  private static PrivateKey getPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    File file = new File(Main.class.getClassLoader().getResource("private.p8").getFile());
+    byte[] bytesArray = FileUtils.readFileToByteArray(file);
+
+    return KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(bytesArray));
+  }
+
+  public IdentityDecryptKey getDecryptKey() {
+    return decryptKey;
+  }
+
+  public EncryptedVerifiers getVerifiers() {
+    return verifiers;
+  }
+
+  public EncryptedVerifiers getPVerifiers() {
+    return pVerifiers;
+  }
+
+  public PseudonymDecryptKey getPDecryptKey() {
+    return pDecryptKey;
+  }
+
+  public PseudonymClosingKey getPClosingKey() {
+    return pClosingKey;
+  }
 }
--- src/main/java/nl/logius/resource/pp/example/Main.java
+++ src/main/java/nl/logius/resource/pp/example/Main.java
@@ -4,7 +4,6 @@
  * Copyright: Logius (2018)
  * @author: Bram van Pelt 
  */
-
 package nl.logius.resource.pp.example;
 
 import java.io.File;
@@ -18,78 +17,77 @@ import nl.logius.resource.pp.util.DecryptUtil;
 import org.apache.commons.io.FileUtils;
 
 public class Main {
-	
-	public static void main(String [] args)
-	{	
-		try 
-		{
-			// In dit voorbeeld werken we me langere sleutels dan normaal wordt gebruikt in Java. 
-			// Hiervoor moet een aanpassing tijdelijk gemaakt worden aan de java security opties. Dit gebeurd in de bijgevoegde methode
-			// Normaal kan hiervoor een patch van Oracle worden geinstalleerd. 
-			// voor java 1.8 is dat http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
-			fixKeyLength();
-			
-			// ook bouncycastle moet worden geregistreerd als JAVA security provider. Normaal wordt dit op de server gedaan voordat de software wordt gerunned.
-			Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-			
-			// laad de sleutels in het geheugen, normaal komen deze uit een keystore
-			KeyUtil keys = new KeyUtil(); 
-			
-			//simuleer een EncryptedID (ei) en EncryptedPseudonym (ep)
+
+  public static void main(String[] args) {
+    try {
+      // In dit voorbeeld werken we me langere sleutels dan normaal wordt gebruikt in Java. 
+      // Hiervoor moet een aanpassing tijdelijk gemaakt worden aan de java security opties. Dit gebeurd in de bijgevoegde methode
+      // Normaal kan hiervoor een patch van Oracle worden geinstalleerd. 
+      // voor java 1.8 is dat http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
+      fixKeyLength();
+
+      // ook bouncycastle moet worden geregistreerd als JAVA security provider. Normaal wordt dit op de server gedaan voordat de software wordt gerunned.
+      Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+      // laad de sleutels in het geheugen, normaal komen deze uit een keystore
+      KeyUtil keys = new KeyUtil();
+
+      //simuleer een EncryptedID (ei) en EncryptedPseudonym (ep)
       File eiFile = new File(Main.class.getClassLoader().getResource("signed/900095222-2-4-I.txt").getFile());
       String ei = FileUtils.readFileToString(eiFile, "UTF-8");
       File epFile = new File(Main.class.getClassLoader().getResource("signed/900095222-2-4-P.txt").getFile());
-			String ep = FileUtils.readFileToString(epFile, "UTF-8");
-			
-			//Pre-load complete, Decrypt de ei en ep
-			String simBsn = DecryptUtil.getIdentity(ei,keys.getDecryptKey(), keys.getVerifiers());
-			String simPseudo = DecryptUtil.getPseudonym(ep, keys.getPDecryptKey(), keys.getPClosingKey(), keys.getPVerifiers());
-			
-			//Doe er iets nuttigs mee ;)
-			System.out.println("Identity:" + simBsn);
-			System.out.println("Pseudo:" + simPseudo);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void fixKeyLength() {
-	    String errorString = "Failed manually overriding key-length permissions.";
-	    int newMaxKeyLength;
-	    try {
-	        if ((newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES")) < 256) {
-	            Class c = Class.forName("javax.crypto.CryptoAllPermissionCollection");
-	            Constructor con = c.getDeclaredConstructor();
-	            con.setAccessible(true);
-	            Object allPermissionCollection = con.newInstance();
-	            Field f = c.getDeclaredField("all_allowed");
-	            f.setAccessible(true);
-	            f.setBoolean(allPermissionCollection, true);
+      String ep = FileUtils.readFileToString(epFile, "UTF-8");
 
-	            c = Class.forName("javax.crypto.CryptoPermissions");
-	            con = c.getDeclaredConstructor();
-	            con.setAccessible(true);
-	            Object allPermissions = con.newInstance();
-	            f = c.getDeclaredField("perms");
-	            f.setAccessible(true);
-	            ((Map) f.get(allPermissions)).put("*", allPermissionCollection);
+      //Pre-load complete, Decrypt de ei en ep
+      String simBsn = DecryptUtil.getIdentity(ei, keys.getDecryptKey(), keys.getVerifiers());
+      String simPseudo = DecryptUtil.getPseudonym(ep, keys.getPDecryptKey(), keys.getPClosingKey(), keys.getPVerifiers());
 
-	            c = Class.forName("javax.crypto.JceSecurityManager");
-	            f = c.getDeclaredField("defaultPolicy");
-	            f.setAccessible(true);
-	            Field mf = Field.class.getDeclaredField("modifiers");
-	            mf.setAccessible(true);
-	            mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-	            f.set(null, allPermissions);
+      //Doe er iets nuttigs mee ;)
+      System.out.println("Identity:" + simBsn);
+      System.out.println("Pseudo:" + simPseudo);
 
-	            newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES");
-	        }
-	    } catch (Exception e) {
-	        throw new RuntimeException(errorString, e);
-	    }
-	    if (newMaxKeyLength < 256)
-	        throw new RuntimeException(errorString); // hack failed
-	}
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void fixKeyLength() {
+    final String ERRORMESSAGE = "Failed manually overriding key-length permissions.";
+    int newMaxKeyLength;
+    try {
+      if ((newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES")) < 256) {
+        Class c = Class.forName("javax.crypto.CryptoAllPermissionCollection");
+        Constructor con = c.getDeclaredConstructor();
+        con.setAccessible(true);
+        Object allPermissionCollection = con.newInstance();
+        Field f = c.getDeclaredField("all_allowed");
+        f.setAccessible(true);
+        f.setBoolean(allPermissionCollection, true);
+
+        c = Class.forName("javax.crypto.CryptoPermissions");
+        con = c.getDeclaredConstructor();
+        con.setAccessible(true);
+        Object allPermissions = con.newInstance();
+        f = c.getDeclaredField("perms");
+        f.setAccessible(true);
+        ((Map) f.get(allPermissions)).put("*", allPermissionCollection);
+
+        c = Class.forName("javax.crypto.JceSecurityManager");
+        f = c.getDeclaredField("defaultPolicy");
+        f.setAccessible(true);
+        Field mf = Field.class.getDeclaredField("modifiers");
+        mf.setAccessible(true);
+        mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+        f.set(null, allPermissions);
+
+        newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES");
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(ERRORMESSAGE, e);
+    }
+    if (newMaxKeyLength < 256) {
+      throw new RuntimeException(ERRORMESSAGE); // hack failed
+    }
+  }
 
 }
