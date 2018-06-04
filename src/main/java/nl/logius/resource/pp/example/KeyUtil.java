a7cf91ecad1e71ae8504d2f90321696600ccdf70
Ewald Wasscher <ewald.wasscher@kpn.com>
2018-06-04 09:24:05 +0200

    Removed try-catch from KeyUtil.java
    Error handling in the catch blocks was not implemented. Removing try-catch ensures error messages will be shown and therefore facilitates troubleshooting. For an example application like this showing stack traces instead of user-friendly error messages is more than acceptable and for sure preferable to hiding the cause of errors.

--- src/main/java/nl/logius/resource/pp/example/KeyUtil.java
+++ src/main/java/nl/logius/resource/pp/example/KeyUtil.java
@@ -9,9 +9,12 @@ package nl.logius.resource.pp.example;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 
 import nl.logius.resource.pp.crypto.CMS;
@@ -32,51 +35,40 @@ public class KeyUtil {
 	private String IDENTITY_POINT = "AmUppru04ghsI/FvbvV59eoX3lCUWlMAZKu1pPdlvixch5avV+aFwQg=";
 	private String PSEUDONYM_POINT = "A9GtKDUn++nl2NWtN4F/2id1gmBhxn4I6Qr9BfeMN+fjNuXGvE79qHc=";
 	
-	public KeyUtil()
+	public KeyUtil() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
 	{
          getIdentityKeys();
          getPseudoKeys();
 	}
 	
-	private void getIdentityKeys()
+	private void getIdentityKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
 	{
         // Convert P7 key to PEM
-        try (final InputStream is = KeyUtil.class.getResourceAsStream("/p7/ID-4.p7")) {
+        final InputStream is = KeyUtil.class.getResourceAsStream("/p7/ID-4.p7");
             String identityKeyPem = CMS.read(getPrivateKey(), is);
             // Convert PEM to IdentityDecryptKey
             decryptKey = DecryptKey.fromPem(identityKeyPem, IdentityDecryptKey.class);
             // Derive verifier (for signature verifying) from key
             verifiers = decryptKey.toVerifiers(IDENTITY_POINT);
-        }        
-        catch (Exception e) {
-			// TODO: handle exception
-		}
 	}
 	
-	private void getPseudoKeys()
+	private void getPseudoKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
 	{   
-        try (final InputStream is = KeyUtil.class.getResourceAsStream("/p7/PD-4.p7")) {
-        	String pseudoKeyPem = CMS.read(getPrivateKey(), is);
+        final InputStream isPd = KeyUtil.class.getResourceAsStream("/p7/PD-4.p7");
+        	String pseudoKeyPem = CMS.read(getPrivateKey(), isPd);
             // Convert PEM to IdentityDecryptKey
         	pDecryptKey = DecryptKey.fromPem(pseudoKeyPem, PseudonymDecryptKey.class);
             // Derive verifier (for signature verifying) from key
             pVerifiers = pDecryptKey.toVerifiers(PSEUDONYM_POINT);
-        }        
-        catch (Exception e) {
-			// TODO: handle exception
-		}
         
-        try (final InputStream is = KeyUtil.class.getResourceAsStream("/p7/PC-4.p7")) {
-        	String pseudoClosingKeyPem = CMS.read(getPrivateKey(), is);
+        final InputStream isPc = KeyUtil.class.getResourceAsStream("/p7/PC-4.p7");
+        	String pseudoClosingKeyPem = CMS.read(getPrivateKey(), isPc);
             // Convert PEM to IdentityDecryptKey
         	pClosingKey = DecryptKey.fromPem(pseudoClosingKeyPem, PseudonymClosingKey.class);
-        }        
-        catch (Exception e) {
-			// TODO: handle exception
-		}
+
 	}
 	
-	private static PrivateKey getPrivateKey() throws Exception {
+	private static PrivateKey getPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
 			File file = new File(Main.class.getClassLoader().getResource("private.p8").getFile());
     	byte[] bytesArray = FileUtils.readFileToByteArray(file);
 
